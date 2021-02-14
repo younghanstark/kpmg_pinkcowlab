642629374144d208e8b5cdbc1cbd2703fa583f64
deok <39942335+ziinxed@users.noreply.github.com>
2021-02-15 00:10:15 +0900

    socketio2

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ io.on("connection", (socket) => {
     const {url} = socket.request;
     console.log(`connected : ${url} : server`);
     socket.on("data", (data) => {
-        console.log('sent')
+     
        
     })
     
