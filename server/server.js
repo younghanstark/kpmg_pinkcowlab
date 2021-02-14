6457f6864a72de06220ae7c7b79de9f293fb5711
deok <39942335+ziinxed@users.noreply.github.com>
2021-02-15 00:05:45 +0900

    socketio

--- server/server.js
+++ server/server.js
@@ -1,37 +1,24 @@
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const socketio = require('socket.io');
+
 const app = express();
-const httpServer = http.createServer(app);
+
+app.get("/", (req, res) => res.sendFile(`${__dirname}/views/index.html`));
+const server = http.createServer(app);
+
+const io = socketio(server);
+io.on("connection", (socket) => {
+    const {url} = socket.request;
+    console.log(`connected : ${url} : server`);
+    socket.on("data", (data) => {
+        console.log('sent')
+       
+    })
+    
+})
 
 const PORT = process.env.PORT || 3000;
 
-const wsServer = new WebSocket.Server({ server: httpServer });
-
-// array of connected websocket clients
-let connectedClients = [];
-
-wsServer.on('connection', (ws, req) => {
-    console.log('Connected');
-    // add new connected client
-    connectedClients.push(ws);
-    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
-    ws.on('message', data => {
-        // send the base64 encoded frame to each connected ws
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
-            } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
-            }
-        });
-    });
-});
-
-// HTTP stuff
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
-});
-httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
+server.listen(3000, () => console.log(`listening on port : ${PORT}`));
