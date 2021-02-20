bbdc298652ef2a5443384af3a19bd51202f3a60f
Young-Han Park <younghanpark@yonsei.ac.kr>
2021-02-20 22:38:32 +0900

    demo page more improvement
    added some animation, frontend features

---
+++ server/views/demopage.js
@@ -0,0 +1,19 @@
+var chkbtn_phase1 = document.getElementById("phase1-next-button");
+chkbtn_phase1.onclick = function() {
+    var cameraselect = document.getElementById("devices");
+    var cur_option = cameraselect.options.selectedIndex;
+    var cur_value = cameraselect.options[cur_option].value;
+    if (cur_value == "notselected") {
+        var div_alert = document.getElementById("select-alert");
+        div_alert.style.display = "block";
+        div_alert.classList.remove("shake");
+        div_alert.offsetWidth = div_alert.offsetWidth;
+        div_alert.classList.add("shake");
+    }
+    else {
+        var div_phase1 = document.getElementById("phase1");
+        div_phase1.style.display = "none";
+        var div_phase2 = document.getElementById("phase2");
+        div_phase2.style.display = "block";
+    }
+}
